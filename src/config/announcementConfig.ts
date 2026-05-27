d5c0dd12ee6093b67b5064befd2362a543c009c4
zeroWalk <zengruiji@163.com>
2026-05-27 16:09:48 +0800

    修改公告

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,10 +5,12 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "欢迎来到我的博客！根据时间和天气炎热程度随机刷新几篇文章。",
 
 	// 是否允许用户关闭公告
 	closable: true,
+
+	type: "info",
 
 	link: {
 		// 启用链接
